491fb46955928546e5cdd84aade5b6e178963848
Juliano <juliano.oliveira@zappts.com>
2021-11-17 14:42:50 -0300

    conclusão do desafio

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -10,6 +10,11 @@ describe('Twitter clone - LOGIN', () => {
             fixture: 'logo-feed'
         }).as('cloudinary')
     });
+
+    after(() => {
+        cy.excluirPublicacao()
+    })
+
     it('Ao autenticar com credenciais válidas, deve ser direcionado o feed', () => {
         cy.login()
             .visit('/')
@@ -20,5 +25,18 @@ describe('Twitter clone - LOGIN', () => {
             .should('have.text', 'Home')
             .get('.sc-fznyAO.dLpyHG')
             .should('be.visible')
+    });
+
+    it.only('Realizar Tweet, este deve ficar visível no feed', () => {
+        let inputTweet = 'Decolar na carreira de QA, ser um especialista na área \\0/'
+        cy.login()
+            .visit('/')
+
+        cy.realizarTweet(inputTweet)
+
+        cy.intercept('POST', 'https://twitter-clone-example-backend.herokuapp.com/')
+            .as('postFeed')
+
+        cy.checarPublicacao(inputTweet)
     });
 });
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -12,5 +12,14 @@ declare namespace Cypress {
 
         token(): void
 
+        /**
+       * @example cy.realizarTweet(string) Necessário enviar string (texto a ser postado)
+       */
+        realizarTweet()
+        /**
+      * @example cy.checarPublicacao(string) Necessário enviar string (texto a ser comparado)
+      */
+        checarPublicacao()
+
     }
 }
