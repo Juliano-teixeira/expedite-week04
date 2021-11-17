34ec7d2b8c0b6b12d3c132dc48e569376f6d9037
Juliano <juliano.oliveira@zappts.com>
2021-11-17 14:51:36 -0300

    conclusão do desafio

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -26,17 +26,30 @@ describe('Twitter clone - LOGIN', () => {
             .get('.sc-fznyAO.dLpyHG')
             .should('be.visible')
     });
+});
+
+describe('Twitter clone - POSTAGEM', () => {
+    before(() => {
+        cy.intercept({
+            method: 'GET',
+            hostname: 'res.cloudinary.com'
+        }, {
+            statusCode: 200,
+            fixture: 'logo-feed'
+        }).as('cloudinary')
+    });
+
+    after(() => {
+        cy.excluirPublicacao()
+    })
 
     it.only('Realizar Tweet, este deve ficar visível no feed', () => {
         let inputTweet = 'Decolar na carreira de QA, ser um especialista na área \\0/'
         cy.login()
             .visit('/')
-
         cy.realizarTweet(inputTweet)
-
         cy.intercept('POST', 'https://twitter-clone-example-backend.herokuapp.com/')
             .as('postFeed')
-
         cy.checarPublicacao(inputTweet)
     });
 });
